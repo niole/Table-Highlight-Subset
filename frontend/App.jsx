6b6aaee360c534ef63ca7833eadf4ad0e7cecfd0
Niole Nelson <niolenelson@gmail.com>
2015-07-06 15:18:11 -0400

    comments

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -5,6 +5,18 @@ var React = require('react');
 var Table = require('./Table.jsx');
 var Data = require('./Data.jsx');
 
+/*
+ * subset - subset of data you want to highlight
+ * functionality - "highlight" or "subset"
+ * getColHeader - callback returns a column header string once clicked on
+ * getRange - callback returns min and max of all data in clicked on column
+ * data - data to visualize {attr1: int, attr2: int...}
+ * titles - column headers {title: "title_which_matches_attr_keys_in_data,
+ *                          display: "title you want to show on table, defaults
+ *                                    to null and shows titles.title"}
+ * display - "none" or "block" to show header which displays column being filtered
+ * */
+
 var App = React.createClass({
 
   getInitialState: function(){
